d9b76e342de1e19eed1320b80648dfdbf4acf86c
shakingWaves <shakingWaves@aliyun.com>
2019-02-09 17:53:50 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ffmpegH264Opt
---
+++ libavformat/allformats.c
@@ -0,0 +1,57 @@
+/*
+ * Register all the formats and protocols
+ * Copyright (c) 2000, 2001, 2002 Fabrice Bellard
+ *
+ * This file is part of FFmpeg.
+ *
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include "avformat.h"
+#include "rtp.h"
+#include "rdt.h"
+#include "url.h"
+#include "version.h"
+#include "config.h"
+#define REGISTER_MUXER(X, x)                                            \
+    {                                                                   \
+        extern AVOutputFormat ff_##x##_muxer;                           \
+        if (CONFIG_##X##_MUXER)                                         \
+            av_register_output_format(&ff_##x##_muxer);                 \
+    }
+
+#define REGISTER_DEMUXER(X, x)                                          \
+    {                                                                   \
+        extern AVInputFormat ff_##x##_demuxer;                          \
+        if (CONFIG_##X##_DEMUXER)                                       \
+            av_register_input_format(&ff_##x##_demuxer);                \
+    }
+
+#define REGISTER_MUXDEMUX(X, x) REGISTER_DEMUXER(X, x)
+
+void av_register_all(void)
+{
+    static int initialized;
+
+    if (initialized)
+        return;
+
+    avcodec_register_all();
+
+    /* (de)muxers */
+    REGISTER_MUXDEMUX(H264,             h264);
+
+    initialized = 1;
+}
